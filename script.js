1e09ce99bfdd6ca3e13827d07dd86d25af81325a
Sanketpatil012 <122162948+Sanketpatil012@users.noreply.github.com>
2025-02-27 12:39:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
 function isSameType(value1, value2) {
-     if (Number.isNaN(num1) && Number.isNaN(num2)) {
-        return true;
-    }
+  if (Number.isNaN(value1) && Number.isNaN(value2)) {
+    return true;
+  }
     
-    
-    if (Number.isNaN(num1) || Number.isNaN(num2)) {
-        return false;
-    }
+  if (Number.isNaN(value1) || Number.isNaN(value2)) {
+    return false;
+  }
 
-    
-    return typeof value1 === typeof value2;
-	}
+  return typeof value1 === typeof value2;
 }
+
 // Do not change the code below
 let value1 = prompt("Enter Start of the Range.");
 let value2 = prompt("Enter End Of the Range.");
 
 // Alert the result
 alert(isSameType(value1, value2));
-
-
